5b515f7132ac06b5dbcd1b5e6ede8c93a1929651
Devin Coleman <croilders48@gmail.com>
2024-12-20 16:30:38 -0800

    hmm

--- Codewars 7kyu/sumOfTwoString.js
+++ Codewars 7kyu/sumOfTwoString.js
@@ -1,3 +1,10 @@
-function longest(s1, s2) {  
-    return [...(new Set(s1 + s2))].sort().join('');
-  }
+function longest(s1, s2) {
+    // prep
+    //P: always given 2 strings, only ever strings
+    //R: a new str where the two str are add total and sorted without any doubles
+    //E: s1="hg", s2="ba" === "abhg"
+    //P: use a set of the two string then return the str sorted
+   let k = [...(new Set(s1 + s2))].sort().join('')
+   return k
+   
+   }
