15c4ffa527c8cafa4d0820660373180b4c3e8445
Devin Coleman <croilders48@gmail.com>
2025-01-16 04:42:27 -0800

    anotha one

---
+++ Codewars 7kyu/reverse.js
@@ -0,0 +1,6 @@
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+function reverseWords(str){
+    let f = str.split(' ')
+    return f.reverse().join(' ')
+  }
