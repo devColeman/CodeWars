cd0eecdb5260a2f9727f92f064295d82518d3855
Devin Coleman <croilders48@gmail.com>
2025-02-17 04:18:23 -0800

    anotha one

---
+++ Codewars 6kyu/array.Diff.js
@@ -0,0 +1,11 @@
+function arrayDiff(a, b) {
+    // P: given two arrays with integars
+    // R: return the integars from the the first list that are not in the second list then return the first list
+    // E: a = [1, 2] b = [2] => [1], a = [1, 2, 2, 2, 3] b = [2] => [1,3]
+    // P: use a filter on the a array, then maybe use includes to remove the items that are not in b
+    
+    return a.filter((e) => {
+      return !b.includes(e)
+    })
+    
+  }
