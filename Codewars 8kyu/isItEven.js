4d9d23213633195e0a23b7964116f0f76e58a980
Devin Coleman <croilders48@gmail.com>
2025-02-18 15:31:16 -0800

    anotha one

---
+++ Codewars 8kyu/isItEven.js
@@ -0,0 +1,8 @@
+function testEven(n) {
+    // P: given integar
+    // R: return either true or false
+    // E: 0 = true, .5 = false
+    // P: i could use a % 2 === 0 to check if its even
+    
+    return n % 2 === 0 ? true : false;
+  }
