03b31a4ff420602800226f1cd771dd548d8d2599
Devin Coleman <croilders48@gmail.com>
2025-02-21 07:21:43 -0800

    anotha one

---
+++ Codewars 8kyu/parseNiceIntFromCharProblem.js
@@ -0,0 +1,8 @@
+function getAge(inputString){
+    // P: given a string
+    // R: return a number from a string
+    // E: "9 years old" => 9
+    // P: maybe return string[0]?
+    return Number(inputString[0])
+    
+  }
