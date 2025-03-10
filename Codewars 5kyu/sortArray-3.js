e13f3b0afea372cfbc9393c0e37ab7efa1ff77ae
Devin Coleman <croilders48@gmail.com>
2025-03-09 19:37:15 -0700

    anotha one

--- Codewars 5kyu/sortArray-3.js
+++ Codewars 5kyu/sortArray-3.js
@@ -1,20 +1,19 @@
 // input: courses - array of course-names "name-yymm"
 // output: sorted by "yymm", then "name"
 sortme = function( courses ){
-    // P: given a array of string
-    // R: return the array sort but the yymm then the name
-    // E: ["web-1305","site-1305","web-1304","site-1304"] => ["site-1304", "web-1304", "site-1305", "web-1305"]
-    // P: first we need to sort by the number that is in the item
-    let k = courses.sort((a,b) => {
-      
-     a=a.split('-')
-      console.log(typeof Number(a[1]))
-                                  
-                                  
-                                  }
-                        
-                        
-                        )
-    console.log(k)
-  }
+  // P: given a array of string
+  // R: return the array sort but the yymm then the name
+  // E: ["web-1305","site-1305","web-1304","site-1304"] => ["site-1304", "web-1304", "site-1305", "web-1305"]
+  // P: first we need to sort by the number that is in the item
+ 
   
+  return courses.sort((a,b) => {
+    
+    let [nameA, yymmA] = a.split('-')
+    let [nameB, yymmB] = b.split('-')
+
+    return yymmA - yymmB || nameA.localeCompare(nameB)
+      
+    
+     
+  })}
