3c7d801230294a5074d3f6ba8e099477203aad7c
Devin Coleman <croilders48@gmail.com>
2025-03-16 05:30:56 -0700

    anotha one

---
+++ Codewars 8kyu/whatIsBetween.js
@@ -0,0 +1,14 @@
+function between(a, b) {
+    // P: given two numbes
+    // R: return a array
+    // E: 3,5 => [3,4,5]
+    // P: use a for loop to push into a arrayh
+     console.log(a)
+     console.log(b)
+     let k = []
+     for(let i = a; i <= b; i++){
+       k.push(i)
+     }
+     
+    return k
+   }
