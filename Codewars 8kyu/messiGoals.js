09320b4ae1fb3625050c64769727b8ca51089363
Devin Coleman <croilders48@gmail.com>
2025-03-17 06:37:17 -0700

    anotha one

---
+++ Codewars 8kyu/messiGoals.js
@@ -0,0 +1,7 @@
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // P: given 3 integers
+    // R: return a number
+    // E: 0,5,6 => 11, 6,7,4 => 17
+    // P: add them together then return
+   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+ }
