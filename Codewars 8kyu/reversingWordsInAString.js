93101ec1c362b158c7520851bd73369ba4747c55
Devin Coleman <croilders48@gmail.com>
2025-03-23 23:36:34 -0700

    anotha one

---
+++ Codewars 8kyu/reversingWordsInAString.js
@@ -0,0 +1,9 @@
+function reverse(string){
+    // P: given a string
+    // R: return a string revered
+    // E: 'Hello Bob' => 'Bob Hello'
+    // P:  split the string bye the space then reverse them
+  
+ return string.split(' ').reverse().join(' ')
+ 
+}
