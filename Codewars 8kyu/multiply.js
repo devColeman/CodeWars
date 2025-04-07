47a790eb3a5d5a97bc745f2bd02e2636f454aae7
Devin Coleman <croilders48@gmail.com>
2025-04-07 07:53:26 -0700

    another one

---
+++ Codewars 8kyu/multiply.js
@@ -0,0 +1,8 @@
+function multiply(a, b){
+    // P: given two integers
+    // R: return one single integer
+    // E: 5, 6 => 30, 3, 3 => 9
+    // P: just return the two integers multiplied by each other
+    
+    return a * b
+  }
