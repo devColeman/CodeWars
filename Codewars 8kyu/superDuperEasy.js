a0a440029646c304d2a2f9d028714a526231e8ae
Devin Coleman <croilders48@gmail.com>
2025-04-13 11:00:12 -0700

    anotha one

---
+++ Codewars 8kyu/superDuperEasy.js
@@ -0,0 +1,14 @@
+function problem(x){
+    // P: given a integer
+    // R: return a number
+    // E: 5 => 256
+    // P: use * then add 6
+    
+     if ( typeof x === 'number') {
+       return (x * 50) + 6
+      
+    }else {
+       return "Error";
+    }
+   
+  }
