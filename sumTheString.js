268d0ca8dceecfec1f0c068d2edb29b3f8dedc3f
Devin Coleman <croilders48@gmail.com>
2025-04-14 10:10:24 -0700

    anotha one

---
+++ sumTheString.js
@@ -0,0 +1,9 @@
+function sumStr(a,b) {
+    // P: givne two strings
+    // R: return the sum of the two string if both are number if there is no number count
+    // count it as a zero
+    // E: '10' '53' => 63
+    // P: use Number on the both of them
+    return (Number(a) + Number(b)).toString()
+    
+  }
