67876cbf75b78260a9bffd2a656814e96b9e9bd8
Devin Coleman <croilders48@gmail.com>
2025-05-26 15:40:51 -0700

    anotha oen

---
+++ barkingMad.js
@@ -0,0 +1,15 @@
+function Dog (breed) {
+  this.breed = breed;
+  this.bark = ()=> {
+    return 'Woof'
+  }
+  
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
