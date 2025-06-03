680d24a928bd7cb6a996d6b0fa2100d3ac5a3138
Devin Coleman <croilders48@gmail.com>
2025-06-03 12:20:51 -0700

    updated

--- Codewars 7kyu/divideAndConquer.js
+++ Codewars 7kyu/divideAndConquer.js
@@ -1,7 +1,8 @@
 function divCon(x){
-    let f = x.filter((num) => (typeof num) == 'string' )
-    let k = x.filter((num) => (typeof num) == 'number' )
-    f = f.reduce((acc, curr) => Number(acc) + Number(curr))
-    k = k.reduce((acc, curr) => Number(acc) + Number(curr))
-    return k - f
-    }
+    console.log(x)
+  let f = x.filter((num) => (typeof num) == 'string' )
+  let k = x.filter((num) => (typeof num) == 'number' )
+  f = f.reduce((acc, curr) => Number(acc) + Number(curr), 0)
+  k = k.reduce((acc, curr) => Number(acc) + Number(curr), 0)
+  return k - f
+  }
