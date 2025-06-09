00f0189aad50feccef0f7315806380fa1683168d
Devin Coleman <croilders48@gmail.com>
2025-06-09 12:17:23 -0700

    anotha one

---
+++ Codewars 8kyu/twiceAdOld.js
@@ -0,0 +1,8 @@
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+    // given two numbers
+    // return a number
+     // maybe just get the floor of the dads age by the sons age
+     // return 36 / 7
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+   
+   }
