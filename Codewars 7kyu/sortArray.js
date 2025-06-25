793f5034488a8325265cb193a7c2eb47aeff045f
Devin Coleman <croilders48@gmail.com>
2025-06-25 12:12:48 -0700

    anotha one

---
+++ Codewars 7kyu/sortArray.js
@@ -0,0 +1,8 @@
+// input: names - unsorted array
+// output: sorted array
+sortme = function( names ){
+    // P: given a array
+    // R: return a array
+    // E: ['one', 'two', 'three' ] => ["one", "three", "two"]
+    return names.sort()
+  }
