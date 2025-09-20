e2fd384ddc182eaf7d78a51b86e8546dffd0a49b
Devin Coleman <croilders48@gmail.com>
2025-09-19 18:07:06 -0700

    anotha one

---
+++ Codewars 8kyu/ensureQuestion.js
@@ -0,0 +1,8 @@
+function ensureQuestion(s) {
+  // P: given a string
+  // R: return a string
+  // E: 'hey' => 'hey?', '' => '?'
+  // P: check if the sting includes a ? if it does return the string if not add a string
+  
+  return s.includes('?') ? s : `${s}?`
+}
