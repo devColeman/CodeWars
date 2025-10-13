8ca8799dde6d5c1bb93f3950852c43e5052e55ee
Devin Coleman <croilders48@gmail.com>
2025-10-13 12:27:40 -0700

    anotha one

---
+++ Codewars 8kyu/helloNameAndWorld.js
@@ -0,0 +1,13 @@
+function hello(name) {
+    // P: given a string
+    // R: return a string
+    // E: 'Bob' => 'Hello, Bob', '' => 'Hello, World'
+    if(name == undefined || name == '' ){
+     return `Hello, World!` 
+    }else{
+      name = name.toLowerCase()
+     let k = name.slice(0,1).toUpperCase()
+     name = name.slice(1)
+      return `Hello, ${k}${name}!`
+    }
+  }
